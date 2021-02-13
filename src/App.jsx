7d1f049f156dfc9b1356d88acf8033b744b7444c
zack <zb01406>
2021-02-13 15:23:21 -0500

    video test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component, useEffect, useRef } from 'react'
 import { Helmet } from 'react-helmet'
+import YoutubeBackground from 'react-youtube-background'
 
 import Video from '../public/starrynight.mp4'
 import Icon from '../public/app-icon.png'
@@ -8,10 +9,10 @@ import './app.css'
 
 const App = () => {
 const playBack = useRef(null)
-useEffect(() => {
-// playBack.playbackRate = 0.25
-playBack.current.playbackRate = 0.75
-})
+// useEffect(() => {
+// // playBack.playbackRate = 0.25
+// playBack.current.playbackRate = 0.75
+// })
 return (
 
 <div className="App">
@@ -27,9 +28,9 @@ return (
     <link rel="icon" href="/favicon.png" />
     <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
   </Helmet>
-  <video id="vid" autostart="true" playsInline autoPlay={true} muted={true} loop ref={playBack} src={Video}
-    type="video/mp4" />
-  <div className="content">
+
+<YoutubeBackground videoId={"9VRZC9z3fNg"}>
+<div className="content">
     <h1>Starbox</h1>
     <p>Apple's least favorite AppStore.</p>
     <a href="https://twitter.com/starbox_app?ref_src=twsrc%5Etfw" className="twitter-follow-button"
@@ -44,6 +45,9 @@ return (
       © {(new Date()).getFullYear()} starbox
     </footer>
   </div>
+</YoutubeBackground>
+
+  
 </div>
 )
 }
